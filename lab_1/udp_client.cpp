c8cc8701e5800244a256c0cc246c8b6741e33e59
artiff3 <artem.ivan456@gmail.com>
2026-02-05 10:24:57 +0700

    update

--- lab_1/udp_client.cpp
+++ lab_1/udp_client.cpp
@@ -14,25 +14,20 @@
 
 int main(int argc, char* argv[]) {
     if (argc != 5) {
-        std::cerr << "Usage: " << argv[0]
-                  << " <server_ip> <server_port> <i> <count>\n";
-        std::cerr << "Example: " << argv[0] << " 127.0.0.1 50000 3 5\n";
         return 1;
     }
 
     const char* server_ip = argv[1];
     uint16_t server_port = static_cast<uint16_t>(std::stoi(argv[2]));
-    int i = std::stoi(argv[3]);       // задержка в секундах и отправляемое число
-    int count = std::stoi(argv[4]);   // сколько раз отправлять
+    int i = std::stoi(argv[3]);
+    int count = std::stoi(argv[4]);
 
-    // 1. Создаем UDP-сокет
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         perror("socket");
         return 1;
     }
 
-    // 2. Заполняем структуру адреса сервера
     sockaddr_in servaddr{};
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(server_port);
@@ -43,7 +38,6 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // 3. Цикл отправки числа i с задержкой i сек
     for (int k = 0; k < count; ++k) {
         std::string msg = std::to_string(i);
 
@@ -57,7 +51,6 @@ int main(int argc, char* argv[]) {
 
         std::cout << "Sent i=" << i << " (" << (k + 1) << "/" << count << ")\n";
 
-        // Получаем ответ от сервера
         char buffer[1024];
         socklen_t len = sizeof(servaddr);
         ssize_t n = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0,
@@ -69,7 +62,6 @@ int main(int argc, char* argv[]) {
         buffer[n] = '\0';
         std::cout << "Response: " << buffer << std::endl;
 
-        // Задержка i секунд
         sleep(i);
     }
 
--- lab_1/udp_server.cpp
+++ lab_1/udp_server.cpp
@@ -13,27 +13,23 @@
 #include <netinet/in.h>
 
 int main() {
-    // 1. Создаем UDP-сокет
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         perror("socket");
         return 1;
     }
 
-    // 2. Заполняем структуру адреса сервера
     sockaddr_in servaddr{};
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(0); // 0 => ядро само выберет свободный порт
+    servaddr.sin_port = htons(0);
 
-    // 3. Привязываем сокет к адресу (bind)
     if (bind(sockfd, reinterpret_cast<sockaddr*>(&servaddr), sizeof(servaddr)) < 0) {
         perror("bind");
         close(sockfd);
         return 1;
     }
 
-    // 4. Узнаём, какой порт реально выделен (getsockname)
     socklen_t addrlen = sizeof(servaddr);
     if (getsockname(sockfd, reinterpret_cast<sockaddr*>(&servaddr), &addrlen) < 0) {
         perror("getsockname");
@@ -46,13 +42,11 @@ int main() {
     std::cout << "Server listening on IP " << ipbuf
               << " port " << ntohs(servaddr.sin_port) << std::endl;
 
-    // 5. Основной цикл обработки датаграмм
     while (true) {
         char buffer[1024];
         sockaddr_in cliaddr{};
         socklen_t cli_len = sizeof(cliaddr);
 
-        // recvfrom: получаем данные и одновременно узнаем IP/порт клиента
         ssize_t n = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0,
                              reinterpret_cast<sockaddr*>(&cliaddr), &cli_len);
         if (n < 0) {
@@ -60,7 +54,7 @@ int main() {
             continue;
         }
 
-        buffer[n] = '\0'; // делаем C-строку
+        buffer[n] = '\0';
         char client_ip[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &cliaddr.sin_addr, client_ip, sizeof(client_ip));
         uint16_t client_port = ntohs(cliaddr.sin_port);
@@ -68,11 +62,9 @@ int main() {
         std::cout << "Received from " << client_ip << ":" << client_port
                   << " -> \"" << buffer << "\"" << std::endl;
 
-        // Преобразуем полученную информацию (пример: добавим текст)
         std::string msg(buffer);
         std::string response = "Server processed i=" + msg;
 
-        // Отправляем обратно клиенту
         ssize_t sent = sendto(sockfd, response.c_str(), response.size(), 0,
                               reinterpret_cast<sockaddr*>(&cliaddr), cli_len);
         if (sent < 0) {
