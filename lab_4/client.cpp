7108b0974e604993434dfed40cee7495a64ea8dd
artiff3 <artem.ivan456@gmail.com>
2026-02-19 11:20:31 +0700

    Попытка реализации 4 лабораторной с протоколом SCTP

--- lab_4/client.cpp
+++ lab_4/client.cpp
@@ -5,12 +5,23 @@
 #include <unistd.h>
 #include <cstring>
 #include <string>
+#include <netinet/sctp.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <atomic>
+
+std::atomic<bool> running(true);
+
+void signal_handler(int /*sig*/) {
+    running = false;
+    std::cout << "\nОстановка..." << std::endl;
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 5) {
         std::cerr << "Использование: " << argv[0] << " <IP> <port> <число> <delay>" << std::endl;
-        std::cerr << "Пример: " << argv[0] << " 127.0.0.1 40715 2 3" << std::endl;
-        std::cerr << "       Отправит число '2' с задержкой 3 сек" << std::endl;
+        std::cerr << "Пример: " << argv[0] << " 127.0.0.1 36329 2 3" << std::endl;
         return 1;
     }
 
@@ -19,29 +30,26 @@ int main(int argc, char* argv[]) {
     int number = std::stoi(argv[3]);
     int delay = std::stoi(argv[4]);
 
-    if (number < 1 || number > 10) {
-        std::cerr << "Число должно быть от 1 до 10!" << std::endl;
-        return 1;
-    }
+    signal(SIGINT, signal_handler);
 
-    if (delay < 1 || delay > 10) {
-        std::cerr << "Задержка должна быть от 1 до 10 секунд!" << std::endl;
-        return 1;
-    }
+    int sock = socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP);
+    if (sock < 0) { perror("socket"); return 1; }
 
-    int sock;
+    struct sctp_initmsg initmsg = {
+        .sinit_num_ostreams = 5,
+        .sinit_max_instreams = 5,
+        .sinit_max_attempts = 5,
+        .sinit_max_init_timeo = 2000
+    };
+    setsockopt(sock, IPPROTO_SCTP, SCTP_INITMSG, &initmsg, sizeof(initmsg));
+
     struct sockaddr_in server_addr;
-
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("socket");
-        return 1;
-    }
-
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
     inet_pton(AF_INET, ip.c_str(), &server_addr.sin_addr);
+
+    std::cout << "Подключение к " << ip << ":" << port << std::endl;
 
     if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("connect");
@@ -49,24 +57,30 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    std::cout << "Подключен к серверу " << ip << ":" << port << std::endl;
-    std::cout << "Отправка числа '" << number << "' с интервалом " << delay
-              << " сек (Ctrl+C для остановки)..." << std::endl;
+    std::cout << "Подключен к " << ip << ":" << port << std::endl;
 
-    while (true) {
-        std::string message = std::to_string(number) + "\n";
-        ssize_t bytes_sent = write(sock, message.c_str(), message.length());
+    std::string message = std::to_string(number) + "\n";
 
-        if (bytes_sent < 0) {
-            perror("write");
+    std::cout << "Отправка '" << number << "' каждые " << delay
+              << "с (Ctrl+C для остановки)..." << std::endl;
+
+    while (running) {
+        ssize_t bytes_sent = sctp_sendmsg(sock, message.c_str(), message.length(),
+                                        NULL, 0, 0, 0, 0, 0, 0);
+
+        if (bytes_sent > 0) {
+            std::cout << "" << bytes_sent << " байт отправлено" << std::endl;
+        } else {
+            perror("sctp_sendmsg");
             break;
         }
 
-        std::cout << "Отправлено: " << number << std::endl;
-
-        sleep(delay);
+        for (int i = 0; i < delay && running; i++) {
+            sleep(1);
+        }
     }
 
+    std::cout << "Отключение..." << std::endl;
     close(sock);
     return 0;
 }
--- lab_4/server.cpp
+++ lab_4/server.cpp
@@ -1,125 +1,52 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/select.h>
 #include <cstring>
-#include <vector>
-#include <map>
+#include <netinet/sctp.h>
+#include <arpa/inet.h>
 
 int main() {
-    int server_fd, client_fd;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len = sizeof(client_addr);
+    int sfd = socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP);
+    if (sfd < 0) { perror("socket"); return 1; }
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        perror("socket");
-        return 1;
+    struct sctp_event_subscribe events = {0};
+    events.sctp_data_io_event = 1;
+    events.sctp_association_event = 1;
+    setsockopt(sfd, SOL_SCTP, SCTP_EVENTS, &events, sizeof(events));
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(0);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sfd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("bind"); return 1;
     }
 
-    int opt = 1;
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    listen(sfd, 5);
 
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = 0;
-
-    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("bind");
-        close(server_fd);
-        return 1;
+    socklen_t addr_len = sizeof(addr);
+    if (getsockname(sfd, (struct sockaddr*)&addr, &addr_len) < 0) {
+        perror("getsockname"); return 1;
     }
 
-    socklen_t addr_len = sizeof(server_addr);
-    getsockname(server_fd, (struct sockaddr*)&server_addr, &addr_len);
-    uint16_t server_port = ntohs(server_addr.sin_port);
-    std::cout << "Сервер запущен на порту: " << server_port << std::endl;
-
-    listen(server_fd, 10);
-
-    std::vector<int> client_sockets;
-    std::map<int, std::pair<std::string, uint16_t>> client_info;
-    fd_set readfds;
-    int max_fd = server_fd;
-
-    std::cout << "Ожидание подключений клиентов..." << std::endl;
+    listen(sfd, 5);
+    std::cout << "SCTP сервер готов на порту: "
+              << ntohs(addr.sin_port) << std::endl;
 
     while (true) {
-        FD_ZERO(&readfds);
+        struct sockaddr_in client_addr = {};
+        socklen_t len = sizeof(client_addr);
+        char buf[256];
+        struct sctp_sndrcvinfo sri = {};
+        int flags = 0;
 
-        FD_SET(server_fd, &readfds);
+        ssize_t n = sctp_recvmsg(sfd, buf, sizeof(buf)-1,
+                               (struct sockaddr*)&client_addr, &len, &sri, &flags);
 
-        for (int sock : client_sockets) {
-            FD_SET(sock, &readfds);
-            if (sock > max_fd) {
-                max_fd = sock;
-            }
-        }
-
-        int activity = select(max_fd + 1, &readfds, NULL, NULL, NULL);
-
-        if (activity < 0) {
-            perror("select");
-            break;
-        }
-
-        if (FD_ISSET(server_fd, &readfds)) {
-            client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
-            if (client_fd >= 0) {
-                char client_ip[INET_ADDRSTRLEN];
-                uint16_t client_port = ntohs(client_addr.sin_port);
-                inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
-
-                std::cout << "Новое подключение от: " << client_ip
-                          << ":" << client_port << std::endl;
-
-                client_info[client_fd] = {std::string(client_ip), client_port};
-
-                client_sockets.push_back(client_fd);
-                if (client_fd > max_fd) {
-                    max_fd = client_fd;
-                }
-            }
-        }
-
-        for (size_t i = 0; i < client_sockets.size(); ) {
-            int sock = client_sockets[i];
-            if (FD_ISSET(sock, &readfds)) {
-                char buffer[256];
-                ssize_t bytes_read = read(sock, buffer, sizeof(buffer) - 1);
-
-                if (bytes_read <= 0) {
-                    auto it = client_info.find(sock);
-                    if (it != client_info.end()) {
-                        std::cout << "Клиент " << it->second.first
-                                  << ":" << it->second.second << " отключился" << std::endl;
-                        client_info.erase(it);
-                    }
-
-                    close(sock);
-                    client_sockets.erase(client_sockets.begin() + i);
-                    if (sock == max_fd) {
-                        max_fd--;
-                    }
-                    continue;
-                }
-
-                buffer[bytes_read] = '\0';
-                std::cout << "Получено от клиента " << client_info[sock].first
-                          << ":" << client_info[sock].second << ": " << buffer << std::endl;
-            }
-            i++;
-        }
+       // if (n > 0 && n != 20) {  // Только данные, НЕ heartbeat
+       //     std::cout << buf[0] << std::endl;
+        //}
     }
-
-    for (int sock : client_sockets) {
-        close(sock);
-    }
-    close(server_fd);
-
-    return 0;
 }
