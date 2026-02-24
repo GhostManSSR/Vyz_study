1b04edb41236c363f05da8855e000aa65492b2f7
artiff3 <artem.ivan456@gmail.com>
2026-02-24 18:13:09 +0700

    реализация 4 лабы протокол SCTP

--- lab_4/client.cpp
+++ lab_4/client.cpp
@@ -6,81 +6,75 @@
 #include <cstring>
 #include <string>
 #include <netinet/sctp.h>
-#include <fcntl.h>
-#include <sys/select.h>
 #include <signal.h>
 #include <atomic>
 
 std::atomic<bool> running(true);
 
-void signal_handler(int /*sig*/) {
+void handler(int) {
     running = false;
-    std::cout << "\nОстановка..." << std::endl;
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 5) {
-        std::cerr << "Использование: " << argv[0] << " <IP> <port> <число> <delay>" << std::endl;
-        std::cerr << "Пример: " << argv[0] << " 127.0.0.1 36329 2 3" << std::endl;
+
+    if (argc != 4) {
+        std::cout << "Usage: client <ip> <port> <number>\n";
         return 1;
     }
 
     std::string ip = argv[1];
-    int port = std::stoi(argv[2]);
-    int number = std::stoi(argv[3]);
-    int delay = std::stoi(argv[4]);
+    int port = atoi(argv[2]);
+    int number = atoi(argv[3]);
 
-    signal(SIGINT, signal_handler);
+    signal(SIGINT, handler);
 
-    int sock = socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP);
-    if (sock < 0) { perror("socket"); return 1; }
-
-    struct sctp_initmsg initmsg = {
-        .sinit_num_ostreams = 5,
-        .sinit_max_instreams = 5,
-        .sinit_max_attempts = 5,
-        .sinit_max_init_timeo = 2000
-    };
-    setsockopt(sock, IPPROTO_SCTP, SCTP_INITMSG, &initmsg, sizeof(initmsg));
-
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
-    inet_pton(AF_INET, ip.c_str(), &server_addr.sin_addr);
-
-    std::cout << "Подключение к " << ip << ":" << port << std::endl;
-
-    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("connect");
-        close(sock);
+    int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP);
+    if (sock < 0) {
+        perror("socket");
         return 1;
     }
 
-    std::cout << "Подключен к " << ip << ":" << port << std::endl;
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &addr.sin_addr);
 
-    std::string message = std::to_string(number) + "\n";
+    std::cout << "Connecting to "
+              << ip << ":" << port << std::endl;
 
-    std::cout << "Отправка '" << number << "' каждые " << delay
-              << "с (Ctrl+C для остановки)..." << std::endl;
+    if (connect(sock, (sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("connect");
+        return 1;
+    }
+
+    std::cout << "Connected\n";
+
+    std::string msg = std::to_string(number) + "\n";
 
     while (running) {
-        ssize_t bytes_sent = sctp_sendmsg(sock, message.c_str(), message.length(),
-                                        NULL, 0, 0, 0, 0, 0, 0);
 
-        if (bytes_sent > 0) {
-            std::cout << "" << bytes_sent << " байт отправлено" << std::endl;
-        } else {
-            perror("sctp_sendmsg");
+        ssize_t sent = sctp_sendmsg(sock, msg.c_str(), msg.size(), NULL, 0, 0, 0, 0, 0, 0);
+
+        if (sent < 0) {
+            perror("send");
             break;
         }
 
-        for (int i = 0; i < delay && running; i++) {
-            sleep(1);
+        char buf[256];
+        struct sctp_sndrcvinfo sri{};
+        int flags = 0;
+
+        int n = sctp_recvmsg(sock, buf, sizeof(buf) - 1,  NULL, 0, &sri, &flags);
+
+        if (n > 0) {
+            buf[n] = 0;
+            std::cout << "Server reply: " << buf;
         }
+
+        sleep(number);
     }
 
-    std::cout << "Отключение..." << std::endl;
+    std::cout << "Disconnecting...\n";
+
     close(sock);
-    return 0;
 }
--- lab_4/server.cpp
+++ lab_4/server.cpp
@@ -3,50 +3,119 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <cstring>
-#include <netinet/sctp.h>
 #include <arpa/inet.h>
+#include <netinet/sctp.h>
+#include <map>
 
 int main() {
-    int sfd = socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP);
-    if (sfd < 0) { perror("socket"); return 1; }
 
-    struct sctp_event_subscribe events = {0};
-    events.sctp_data_io_event = 1;
-    events.sctp_association_event = 1;
-    setsockopt(sfd, SOL_SCTP, SCTP_EVENTS, &events, sizeof(events));
+    int listen_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP);
+    if (listen_fd < 0) {
+        perror("socket");
+        return 1;
+    }
 
-    struct sockaddr_in addr = {};
+    sockaddr_in addr{};
     addr.sin_family = AF_INET;
     addr.sin_port = htons(0);
     addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(sfd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-        perror("bind"); return 1;
+    if (bind(listen_fd, (sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("bind");
+        return 1;
     }
 
-    listen(sfd, 5);
-
-    socklen_t addr_len = sizeof(addr);
-    if (getsockname(sfd, (struct sockaddr*)&addr, &addr_len) < 0) {
-        perror("getsockname"); return 1;
+    if (listen(listen_fd, 10) < 0) {
+        perror("listen");
+        return 1;
     }
 
-    listen(sfd, 5);
-    std::cout << "SCTP сервер готов на порту: "
+    socklen_t len = sizeof(addr);
+    getsockname(listen_fd, (sockaddr*)&addr, &len);
+
+    std::cout << "SERVER STARTED ON PORT: "
               << ntohs(addr.sin_port) << std::endl;
 
+    fd_set master_set, read_set;
+
+    FD_ZERO(&master_set);
+    FD_SET(listen_fd, &master_set);
+
+    int max_fd = listen_fd;
+
+    std::map<int, sockaddr_in> clients;
+
     while (true) {
-        struct sockaddr_in client_addr = {};
-        socklen_t len = sizeof(client_addr);
-        char buf[256];
-        struct sctp_sndrcvinfo sri = {};
-        int flags = 0;
 
-        ssize_t n = sctp_recvmsg(sfd, buf, sizeof(buf)-1,
-                               (struct sockaddr*)&client_addr, &len, &sri, &flags);
+        read_set = master_set;
 
-       // if (n > 0 && n != 20) {  // Только данные, НЕ heartbeat
-       //     std::cout << buf[0] << std::endl;
-        //}
+        if (select(max_fd + 1, &read_set, nullptr, nullptr, nullptr) < 0) {
+            perror("select");
+            break;
+        }
+
+        for (int fd = 0; fd <= max_fd; fd++) {
+
+            if (!FD_ISSET(fd, &read_set))
+                continue;
+
+            if (fd == listen_fd) {
+
+                sockaddr_in client_addr{};
+                socklen_t clen = sizeof(client_addr);
+
+                int client_fd = accept(listen_fd, (sockaddr*)&client_addr, &clen);
+
+                if (client_fd < 0) {
+                    perror("accept");
+                    continue;
+                }
+
+                clients[client_fd] = client_addr;
+
+                std::cout << "Client connected " << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port) << std::endl;
+
+                FD_SET(client_fd, &master_set);
+
+                if (client_fd > max_fd)
+                    max_fd = client_fd;
+            }
+            else {
+
+                char buf[256];
+                int flags = 0;
+                struct sctp_sndrcvinfo sri{};
+
+                int n = sctp_recvmsg(fd, buf, sizeof(buf) - 1, NULL, 0, &sri, &flags);
+
+                if (n <= 0) {
+
+                    auto c = clients[fd];
+
+                    std::cout << "Client disconnected " << inet_ntoa(c.sin_addr) << ":" << ntohs(c.sin_port) << std::endl;
+
+                    close(fd);
+                    FD_CLR(fd, &master_set);
+                    clients.erase(fd);
+
+                    continue;
+                }
+
+                buf[n] = 0;
+
+                int value = atoi(buf);
+
+                std::cout << "Received from " << inet_ntoa(clients[fd].sin_addr) << ":" << ntohs(clients[fd].sin_port) << " -> " << value << std::endl;
+
+                int result = value * 2;
+
+                std::string reply = std::to_string(result) + "\n";
+
+                sctp_sendmsg(fd, reply.c_str(), reply.size(), NULL, 0, 0, 0, 0, 0, 0);
+            }
+        }
     }
+
+    close(listen_fd);
+    return 0;
 }
