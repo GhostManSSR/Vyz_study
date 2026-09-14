ee2b45ca30487e0016aa493e7087948b84cde283
artiff3 <artem.ivan456@gmail.com>
2026-09-14 19:43:11 +0700

    Алгоритм свертки и свертка через фурье

---
+++ lab_1/Lab1/DiscreteFourierTransform.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Numerics;
+
+namespace SortingAlgorithms
+{
+    public class DiscreteFourierTransform
+    {
+        /// <summary>
+        /// Прямое дискретное преобразование Фурье.
+        /// Сложность: O(N^2)
+        /// </summary>
+        public Complex[] Transform(double[] input)
+        {
+            int n = input.Length;
+            Complex[] output = new Complex[n];
+
+            for (int k = 0; k < n; k++)
+            {
+                Complex sum = Complex.Zero;
+
+                for (int j = 0; j < n; j++)
+                {
+                    double angle = -2.0 * Math.PI * k * j / n;
+
+                    Complex w = new Complex(
+                        Math.Cos(angle),
+                        Math.Sin(angle)
+                    );
+
+                    sum += input[j] * w;
+                }
+
+                output[k] = sum;
+            }
+
+            return output;
+        }
+    
+
+        /// <summary>
+        /// Обратное дискретное преобразование Фурье.
+        /// Сложность: O(N^2)
+        /// </summary>
+        public Complex[] InverseTransform(Complex[] input)
+        {
+            int n = input.Length;
+            Complex[] output = new Complex[n];
+            for (int j = 0; j < n; j++)
+            {
+                Complex sum = Complex.Zero;
+                for (int k = 0; k < n; k++)
+                {
+                    double angle = 2.0 * Math.PI * k * j / n;
+                    Complex w = new Complex( Math.Cos(angle), Math.Sin(angle) );
+                    sum += input[k] * w;
+                } 
+                output[j] = sum / n;
+            }
+            return output;
+        }
+    }
+}
