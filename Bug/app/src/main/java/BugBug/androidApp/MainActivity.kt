ca333fb05b1050b1132ffd793bccf38ea448ba1b
Timofey <sadfsdfsdf@gmail.com>
2026-09-15 19:56:44 +0700

    add mainactivity

---
+++ Bug/app/src/main/java/BugBug/androidApp/MainActivity.kt
@@ -0,0 +1,23 @@
+package BugBug.androidApp
+
+import BugBug.androidApp.navigation.AppNavHost
+import BugBug.bug.ui.theme.BugTheme
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BugTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    AppNavHost()
+                }
+            }
+        }
+    }
+}
